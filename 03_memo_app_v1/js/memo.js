24bc4d836d75fdcf04aa9199641853d13d70f7b6
xiudans <84057776+xiudans@users.noreply.github.com>
2022-01-14 15:39:52 +0900

    Update memo.js

--- 03_memo_app_v1/js/memo.js
+++ 03_memo_app_v1/js/memo.js
@@ -17,7 +17,7 @@ function(){
 );
 
 
-//保存
+2.//保存
 function saveLocalStorage(){
     const save = document.getElementById("save");
     save.addEventListener("click",
@@ -27,89 +27,195 @@ function saveLocalStorage(){
             const value = document.getElementById("textMemo").value;
             //値の入力チェック
             if(key =="" || value ==""){
-                window.alert("key,Memoいずれも必要です");
+                Swal.fire({
+                    title:"Memo app"
+                    ,html:"key,Memoいずれも必要です"
+                    ,type:"error"
+                    ,allowOutsideClick:false
+                });
+                
                 return;
             }else{
-                localStorage.setItem(key,value);
-                viewStorage();
-                let w_msg = "LocalStorageに" + key + value +"を保存しました。";
-                window.alert(w_msg);
-                document.getElementById("textKey").value ="";
-                document.getElementById("textMemo").value= "";
+                let w_msg = "LocalStorageに\n[" + key + " " + value + "]\nを保存しますか?" ;
+                Swal.fire({
+                    title:"Memo app"
+                    ,html:w_msg
+                    ,type:"question"
+                    ,showCancelButton:true
+                }).then(function(result){
+
+                
+                if(result.value === true){
+                    localStorage.setItem(key,value);
+                    viewStorage();
+                    let w_msg = "LocalStorageに\n" + key + value +"\nを保存しました。";
+                    Swal.fire({
+                        title:"Memo app"
+                        ,html:w_msg
+                        ,type:"success"
+                        ,allowOutsideClick:false
+                    });
+                    document.getElementById("textKey").value ="";
+                    document.getElementById("textMemo").value= "";
+                }
+        
+                });
+    
             }
         },false
-    );
-
+    )
 };
-
-//削除
+            
+        
+        
+3.//削除
 function delLocalStorage(){
     const del = document.getElementById("del");
     del.addEventListener("click",
     function(e){
         e.preventDefault();
-        let w_sel = "0";
-        w_sel = selectRadioBtn();
+        const chkbox1 = document.getElementsByName("chkbox1");
+        const table1 = document.getElementById("table1");
 
-        if(w_sel === "1"){
-            const key = document.getElementById("textKey").value;
-            const value = document.getElementById("textMemo").value;
-            localStorage.removeItem(key);
-            viewStorage();
-            let w_msg = "LocalStorageから" + key + value +"を削除しました。";
-                window.alert(w_msg);
-                document.getElementById("textKey").value ="";
-                document.getElementById("textMemo").value= "";
-        } 
+        let w_cnt = 0;
+        w_cnt = selectCheckBox(del);
+
+        if(w_cnt >= 1){
+            
+            
+               Swal.fire({
+                    title:"Memo app"
+                    ,html:"LocalStorageから選択されている"+ w_cnt + "件を削除(delete)をしますか?"
+                    ,type:"question"
+                    ,showCancelButton:true
+                }).then(function(result){
+
+                
+                if(result.value === true){
+                
+                for(let i = 0; i < chkbox1.length;i++){
+                    if(chkbox1[i].checked){
+                        localStorage.removeItem(table1.rows[i+1].cells[1].firstChild.data);
+                    } 
+                }                          
+                    viewStorage();
+                                      
+                    Swal.fire({
+                        title:"Memo app"
+                        ,html:"LocalStorageから" + w_cnt +"件を削除(delete)しました。"
+                        ,type:"success"
+                        ,allowOutsideClick:false
+                    });
+                    
+                    document.getElementById("textKey").value ="";
+                    document.getElementById("textMemo").value= "";
+                    
+                }});
+        }
+            
     },false
+    
     );
-};
-//全て削除
+}
+4.//全て削除
 function allClearLocalStorage(){
     const allClear = document.getElementById("allClear");
     allClear.addEventListener("click",
         function(e){
             e.preventDefault();
-            let w_confirm = confirm("LocalStorageのデータ全て削除しますか？");
-            if(w_confirm ===true){
+            
+            Swal.fire({
+                title:"Memo app"
+                ,html:"LocalStorageのデータ全て削除しますか？"
+                ,type:"question"
+                ,showCancelButton:true
+            }).then(function(result){
+
+                
+                if(result.value === true){
+           
+            
                 localStorage.clear();
                 viewStorage();
-                let w_msg = "LocalStorageのデータ全て削除しました";
-                window.alert(w_msg);
+
+                Swal.fire({
+                    title:"Memo app"
+                    ,html:"LocalStorageのデータ全て削除しました"
+                    ,type:"success"
+                    ,allowOutsideClick:false
+                });
+               
+                
                 document.getElementById("textKey").value ="";
                 document.getElementById("textMemo").value= "";
             }
-        },false
+        })},false
     
     );
 };
 
-//データ選択
+5.//データ選択
 function selectTable(){
     const select = document.getElementById("select");
     select.addEventListener("click",
         function(e){
             e.preventDefault;
-            selectRadioBtn();
+            selectCheckBox(select);
         },false
     );
 
 }
-//テーブル選択
-function selectRadioBtn(){
-    let w_sel ="0";
-    const radio1 = document.getElementsByName("radio1");
-    const radio2 = document.getElementById("table1");
+//テーブルからデータ選択
+function selectCheckBox(mode){
+    
+    let w_cnt = 0;
+    const chkbox1 = document.getElementsByName("chkbox1");
+    const table1 = document.getElementById("table1");
+    let w_textKey = "";
+    let w_textMemo = "";
 
-    for(let i = 0;i < radio1.length;i++){
-        if(radio1[i].checked){
-            document.getElementById("textKey").value = table1.rows[i+1].cells[1].firstChild.data;
-            document.getElementById("textMemo").value = table1.rows[i+1].cells[2].firstChild.data;
-            return w_sel ="1";
+    for(let i = 0;i < chkbox1.length;i++){
+        if(chkbox1[i].checked){
+            if(w_cnt === 0){
+                w_textKey = table1.rows[i + 1].cells[1].firstChild.data;
+                w_textMemo = table1.rows[i + 1].cells[2].firstChild.data;       
+            }
+            
+            w_cnt++;
         }
     }
-    window.alert("1つ選択してください");
+    document.getElementById("textKey").value = w_textKey;
+    document.getElementById("textMemo").value= w_textMemo;
+    
+    
+    if(mode === select){
+        if(w_cnt === 1){
+            return w_cnt;            
+        }else{
+            Swal.fire({
+                title:"Memo app"
+                ,html:"1つ選択してください"
+                ,type:"error"
+                ,allowOutsideClick:false
+            });
+        }    
+    }
+
+
+    if(mode === del){
+        if(w_cnt >= 1){
+            return w_cnt;
+        }else{
+            Swal.fire({
+                title:"Memo app"
+                ,html:"１つ以上選択してください"
+                ,type:"error"
+                ,allowOutsideClick:false
+            });           
+        }
+    }
 }
+
 
 function viewStorage(){
     const list = document.getElementById("list");
@@ -118,23 +224,24 @@ function viewStorage(){
 
     for(let i = 0;i < localStorage.length;i++){
         let w_key = localStorage.key(i);
-
         let tr = document.createElement("tr");
         let td1 = document.createElement("td");
         let td2 = document.createElement("td");
         let td3 = document.createElement("td");
+
         list.appendChild(tr);
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
 
-        td1.innerHTML = "<input name='radio1'type ='radio'>";
+        td1.innerHTML = "<input name='chkbox1'type ='checkBox'>";
         td2.innerHTML = w_key;
         td3.innerHTML = localStorage.getItem(w_key);
     }
-
     $("#table1").tablesorter({
         sortList:[[1,0]]
     });
     $("#table1").trigger("update");
 }
+
+            
